07ced3af6257c899ef1558cc2376e8c85559f839
funasty1985 <tony.f.liang@gmail.com>
2019-08-03 20:03:34 +0800

    Initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../HOC/Aux'
+import classes from './Layout.css'
+
+const layout = ( props ) => (
+    <Aux>
+        <div>ToolBar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+    
+);
+
+export default layout;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import Aux from '../../HOC/Aux'
+import Burger from '../../components/Burger/Burger'
+
+class BurgerBuidler extends Component {
+
+    // constructor(props){
+    //     super(props);
+    //     this.state = {...}
+    // }
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat:2
+        }
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <div>Build Controls</div>
+            </Aux>
+        ); 
+    }
+}
+
+export default BurgerBuidler 
