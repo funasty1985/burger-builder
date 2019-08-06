b5eca420e6fc4df2d62d04bc3f8b6c475e5817f1
funasty1985 <tony.f.liang@gmail.com>
2019-08-06 23:05:28 +0800

    Bruger Update

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuidler extends Component {
 
@@ -12,10 +13,10 @@ class BurgerBuidler extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat:2
+            // salad: 1,
+            // bacon: 1,
+            // cheese: 2,
+            // meat:2
         }
     }
 
@@ -23,7 +24,7 @@ class BurgerBuidler extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         ); 
     }
