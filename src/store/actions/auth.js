b3c09097fe47555ed79abe2bd140de7167d81d2f
funasty1985 <tony.f.liang@gmail.com>
2020-03-15 21:47:38 +0800

    Handling Auto-Sign-In with a Saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -47,29 +47,11 @@ export const checkAuthTimeout = (expirationTime) => {
 
 // https://firebase.google.com/docs/reference/rest/auth for firebase auth api 
 export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = {
-            email,
-            password,
-            returnSecureToken: true,
-        }
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2Edi3vbatkKkh6wHY5-tAUMJ7IOpqlpg'; 
-        if (!isSignup){
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB2Edi3vbatkKkh6wHY5-tAUMJ7IOpqlpg'
-        }
-        axios.post(url, authData)
-            .then(res => {  
-                const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000)
-                localStorage.setItem('token', res.data.idToken);
-                localStorage.setItem('expirationDate', expirationDate);
-                localStorage.setItem('userId',res.data.localId)
-                dispatch(authSuccess(res.data.idToken, res.data.localId));
-                dispatch(checkAuthTimeout(res.data.expiresIn));
-            })
-            .catch(err => { 
-                dispatch(authFail({error:'This is a error'}));
-            });
+    return {
+        type: actionTypes.AUTH_USER,
+        email,
+        password, 
+        isSignup
     }
 }
 
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,5 +1,6 @@
 import { put, delay } from 'redux-saga/effects';
 import * as actions from '../actions/index'
+import axios from 'axios';
 
 export function* logoutSaga (action) {
     yield localStorage.removeItem('token');
@@ -11,4 +12,32 @@ export function* logoutSaga (action) {
 export function* checkAuthTimeoutSaga (action) {
     yield delay(action.expirationTime * 1000);
     yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+    yield put(actions.authStart());
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true,
+    };
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB2Edi3vbatkKkh6wHY5-tAUMJ7IOpqlpg'; 
+    if (!action.isSignup){
+        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB2Edi3vbatkKkh6wHY5-tAUMJ7IOpqlpg'
+    }
+
+    try {
+    const  res = yield axios.post(url, authData)  // When generator authUserSaga is called , it will wait for this axios call to finish , just like async await call
+    
+    // yield in synchronous call is optional
+    const  expirationDate = yield new Date(new Date().getTime() + res.data.expiresIn * 1000)
+    yield localStorage.setItem('token', res.data.idToken);
+    yield localStorage.setItem('expirationDate', expirationDate);
+    yield localStorage.setItem('userId',res.data.localId)
+    console.log('It is good here')
+    yield put(actions.authSuccess(res.data.idToken, res.data.localId));
+    yield put(actions.checkAuthTimeout(res.data.expiresIn));
+    }catch(err){ 
+        yield put(actions.authFail({error:'This is a error'}));
+    };
 }
